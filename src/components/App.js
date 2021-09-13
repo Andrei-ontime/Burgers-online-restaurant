559dca3224b731982c8f90f32039ebce366730b2
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-09-13 12:33:52 +0300

    new component AddBurgerForm

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import MenuAdmin from './MenuAdmin';
 
 class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {},
+  };
+
+  addBurger = (burger) => {
+    //1. Делаем копию обьекта state
+    const burgers = { ...this.state.burgers };
+    //2. Добавить новый бургер в переменную burgers
+    burgers[`burger${Date.now()}`] = burger;
+    //3. Записать наш новый обьект burgers в state
+    this.setState({ burgers });
+  };
+
   render() {
     return (
       <div className='burger-paradise'>
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header title='Very Hot Burger' amount={10} hot={true} />
         </div>
         <Order />
-        <MenuAdmin />
+        <MenuAdmin addBurger={this.addBurger} />
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,7 +19,8 @@ class Landing extends React.Component {
   };
 
   goToRestaurant = () => {
-    console.log('[[[');
+    const { url } = this.state;
+    this.props.history.push(`/restaurant/${url}`);
   };
 
   render() {
