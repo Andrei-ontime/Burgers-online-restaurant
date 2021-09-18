5f5c3c8c0e07b208ae5d6cf7c563196ce194762a
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-09-19 00:52:19 +0300

    Component order updated

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,7 @@ class App extends React.Component {
             })}
           </ul>
         </div>
-        <Order />
+        <Order burgers={this.state.burgers} order={this.state.order} />
         <MenuAdmin
           addBurger={this.addBurger}
           loadSampleBurgers={this.loadSampleBurgers}
