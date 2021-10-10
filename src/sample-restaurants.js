37c37f6536a1eec97bd7c96adfe456762f048f7f
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-10-11 00:19:09 +0300

    Relocate restaurants

--- src/sample-restaurants.js
+++ src/sample-restaurants.js
@@ -1,21 +1,31 @@
 // Наши рестораны
 const restaurants = [
   {
-    title: 'Hot Burger Тверская',
-    url: 'hot-burger-tverskaya',
-    id: 1
+    title: 'Hot Burger Западный',
+    url: 'hot-burger-zapadniy',
+    id: 1,
   },
 
   {
-    title: 'Hot Burger Маросейка',
-    url: 'hot-burger-maroseyka',
-    id: 2
+    title: 'Hot Burger Центр',
+    url: 'hot-burger-center',
+    id: 2,
   },
   {
-    title: 'Hot Burger Петровка',
-    url: 'hot-burger-petrovka',
-    id: 3
-  }
+    title: 'Hot Burger Сельмаш',
+    url: 'hot-burger-selmash',
+    id: 3,
+  },
+  {
+    title: 'Hot Burger Военвед',
+    url: 'hot-burger-voenved',
+    id: 4,
+  },
+  {
+    title: 'Hot Burger Ленина',
+    url: 'hot-burger-lenina',
+    id: 5,
+  },
 ];
 
 export default restaurants;
